5906551a0f4dffbe5244aeb295a404241f60309c
KoukiIijima <lle0nonnndebo@gmail.com>
2021-06-28 10:41:43 +0900

    [init]first commit, add speaker query

---
+++ GraphQL/Data/Speaker.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ConferencePlanner.GraphQL.Data
+{
+  public class Speaker
+  {
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(200)]
+    public string Name { get; set; }
+
+    /// <summary>
+    /// biograpy:プロフィール
+    /// </summary>
+    /// <value></value>
+    [StringLength(4000)]
+    public string Bio { get; set; }
+
+    [StringLength(1000)]
+    public virtual string WebSite { get; set; }
+  }
+}
